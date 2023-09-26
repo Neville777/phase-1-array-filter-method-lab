6875a5ea45a1331ac89528e5cd5fcc1738d5e8ef
neville777 <neville.james@student.moringaschool.com>
2023-09-26 05:42:55 +0300

    filter-array

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+// Define the function findMatching
+function findMatching(drivers, name) {
+    // Use the filter method to create a new array with case-insensitive matches
+    return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+  }
+  
+// Define the function fuzzyMatch
+  function fuzzyMatch(drivers, query) {
+    // Use the filter method to create a new array with matching driver names
+    return drivers.filter(driver => driver.startsWith(query));
+  }
+
+  // Define the function matchName
+  function matchName(drivers, name) {
+    // Use the filter method to create a new array with matching driver objects
+    return drivers.filter(driver => driver.name === name);
+  }
